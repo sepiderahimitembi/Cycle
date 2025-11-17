4499fa6914c7628066d8aef2bd4eee98e9e01149
sepiderahimitembi <sepiderahimitembi@gmail.com>
2025-11-17 10:33:52 +0100

    Initial Commit

---
+++ Cycle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cycle
+//
+//  Created by Sepideh Rahimi on 17/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cycle/CycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleApp.swift
+//  Cycle
+//
+//  Created by Sepideh Rahimi on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
